163a2b3a13d58675f854efa24334ee3394a014d9
yijiang <jiangyilf@hotmail.com>
2016-03-16 00:40:03 +0800

    update polyline

--- Html5/H5report/js/H5ComponentPolyline.js
+++ Html5/H5report/js/H5ComponentPolyline.js
@@ -7,40 +7,49 @@ var H5ComponentPolyline = function(name,cfg){
 	var x,y;
 	//创建画布
 	var canvas = document.createElement('canvas');
-	var cxt = canvas.getContext('2d');
-	canvas.width = cxt.width = w;
-	canvas.height = cxt.height = h;
+	var ctx = canvas.getContext('2d');
+	canvas.width = ctx.width = w;
+	canvas.height = ctx.height = h;
 	component.append(canvas);
 
 	//绘制背景   水平网格线
 	
  	var step = 10 //水平网格线 100份 ==> 10
- 	cxt.beginPath();
-    cxt.lineWidth = 1;
-    cxt.strokeStyle = '#AAAAAA';
+ 	ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#AAAAAA';
 
  
 	for(var i=0;i<step+1;i++){
 		y = (h/step) * i
-		cxt.moveTo(0,y);
-		cxt.lineTo(w,y);
+		ctx.moveTo(0,y);
+		ctx.lineTo(w,y);
 	}
 
 	//竖直网格线
 	step = length + 1;
+	var text_w = w/step >> 0;
+
 	for(var i=0;i<step+1;i++){
 		x = (w/step) * i;
-		cxt.moveTo(x,0);
-		cxt.lineTo(x,h);
+		ctx.moveTo(x,0);
+		ctx.lineTo(x,h);
+
+		if(cfg.data[i]){
+			var text = $('<div class="polyline-item-name"></div>');
+			text.text( cfg.data[i][0] );
+			text.css({'width':text_w/2,'top':h/2,'left':x/2+text_w/4});
+			component.append(text);
+		}
 	}
-    cxt.stroke();
-    cxt.closePath();
+    ctx.stroke();
+    ctx.closePath();
 
     //绘制数据
     var canvas = document.createElement('canvas');
-	var cxt = canvas.getContext('2d');
-	canvas.width = cxt.width = w;
-	canvas.height = cxt.height = h;
+	var ctx = canvas.getContext('2d');
+	canvas.width = ctx.width = w;
+	canvas.height = ctx.height = h;
 	component.append(canvas);
 
 	
@@ -50,28 +59,56 @@ var H5ComponentPolyline = function(name,cfg){
 		x = row_w * i + row_w;
 		y = h - h*cfg.data[i][1];
 
-		cxt.beginPath();
-		cxt.arc(x,y,8,0,2*Math.PI,false);
-		cxt.closePath();
-		cxt.fillStyle = "#F00";
-		cxt.fill();
+		ctx.beginPath();
+		ctx.arc(x,y,8,0,2*Math.PI,false);
+		ctx.closePath();
+		ctx.fillStyle = "#F00";
+		ctx.fill();
 	}
-	//连线
-	
-	cxt.lineWidth = 1;
-	cxt.lineStyel = '#F00';
 
-	cxt.moveTo(row_w,h - h*cfg.data[0][1]);
+	//连线 及阴影
+
+	ctx.beginPath();
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = '#F00';
+
+	ctx.moveTo(row_w,h - h*cfg.data[0][1]);
 
 	for(var i=0;i<length;i++){
 		x = row_w * i + row_w;
 		y = h - h*cfg.data[i][1];
-
-		cxt.lineTo(x,y)
-		console.log(x,y);
+		ctx.strokeText(cfg.data[i][1]*100+'%',x - 10,y - 20);
+		ctx.lineTo(x,y);
 	}
-	cxt.stroke();
+	ctx.stroke();
+
+	ctx.lineTo(x,h);
+	ctx.lineTo(row_w,h);
+	ctx.closePath();
+	ctx.fillStyle = "rgba(255, 118, 118, 0.2)";
+	ctx.fill();
 	
+	//绘制数据
+	ctx.beginPath();
+
+	for(var i=0;i<length;i++){
+		x = row_w * i + row_w;
+		y = h - h*cfg.data[i][1];
+		ctx.strokeStyle = cfg.data[i][2] ? cfg.data[i][2] : '#595959';
+		ctx.strokeText(cfg.data[i][1]*100+'%',x - 10,y - 20);
+	}
+	
+	ctx.fill();
+
+	for(var i=0;i<length;i++){
+		x = row_w * i + row_w;
+		y = h - h*cfg.data[i][1];
+		
+		ctx.strokeText(cfg.data[i][1]*100+'%',x - 10,y - 20);
+	}
+	component.addEventListener('onLoad',function(){
+		var timer = window.requestAnimationFrame();
+	});
 
 
 	return component;
