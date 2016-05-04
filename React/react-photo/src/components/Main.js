3c4ec7183883845feb23d3bb889980de93a431af
jiangyi <jiangyi@cdsb.com>
2016-05-04 18:06:34 +0800

    react phone inint

--- React/react-photo/src/components/Main.js
+++ React/react-photo/src/components/Main.js
@@ -17,14 +17,45 @@ imagesDate = (function genImageUrl(imageDateArr){
 })(imagesDate);
 
 
+var ImgFigure = React.createClass({
+    render:function(){
+        return(
+            <figure className="img-figure">
+                <div className="side-front">
+                    <div className="imagebox">
+                    <img src={this.props.data.imageUrl}
+                         alt={this.props.data.title}
+                    />
+                    </div>
+                    <p className="img-title">{this.props.data.title}</p>
+                </div>
+                
+                <figcaption className="side-back">
+                <p className="img-desc">{this.props.data.disc}</p>
+                </figcaption>
+            </figure>
+        )
+    }
+});
+
+
 
 class AppComponent extends React.Component {
   render() {
+    var controllerUnits = [],
+        ImgFigures = [];
+
+    imagesDate.forEach(function(value,index){
+        ImgFigures.push(<ImgFigure data={value} ref={'imgfiguer'+ index} key={index}/>)
+    })
+
     return (
     	<section className="stage">
         	<section className="img-sec">
+                {ImgFigures}
             </section>
         	<nav className="controller-nav">
+                {controllerUnits}
             </nav>
     	</section>
     );
