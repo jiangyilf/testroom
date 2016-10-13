453e00681144bd0ff5381f78cb0aff385ca77391
jiangyi <jiangyi@cdsb.com>
2016-10-13 18:06:32 +0800

    test model

--- Html5/model/model.js
+++ Html5/model/model.js
@@ -1,1 +1,19 @@
-modle.html
+(function(jQuery) {
+	var $ = jQuery;
+
+	$.fn.modelJ = function(argument) {
+		console.log('argument')
+	}
+
+
+})(jQuery)
+
+
+$.modelJ({
+	model:'toast',
+	type:'success',
+	text:'成功',
+	config:{
+		textColor:'#fff'
+	}
+});
